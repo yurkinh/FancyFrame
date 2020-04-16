6c77f10b25c057aafa6ed8f9786858e80cf1c733
Yuriy Holembyovskyy <yuriy.holembyovskyy@gmail.com>
2020-04-16 10:48:58 +0300

    Removed unused properties

--- FancyFrameApp/FancyFrameApp/Control/FancyFrame.cs
+++ FancyFrameApp/FancyFrameApp/Control/FancyFrame.cs
@@ -9,14 +9,8 @@ namespace FancyFrameApp.Control
         readonly SKCanvasView canvas;
         public FancyFrame()
         {
-            RowDefinitions = new RowDefinitionCollection
-        {
-            new RowDefinition { Height = GridLength.Auto }
-        };
-            ColumnDefinitions = new ColumnDefinitionCollection
-        {
-            new ColumnDefinition { Width = GridLength.Auto }
-        };
+            RowDefinitions = new RowDefinitionCollection { new RowDefinition { Height = GridLength.Auto } };
+            ColumnDefinitions = new ColumnDefinitionCollection { new ColumnDefinition { Width = GridLength.Auto } };
 
             canvas = new SKCanvasView()
             {
@@ -28,257 +22,88 @@ namespace FancyFrameApp.Control
             Children.Add(canvas, 0, 0);
         }
 
-        #region Outer
-        public static readonly BindableProperty OuterBackgroundColorProperty =
-            BindableProperty.Create("OuterBackgroundColor",
-                typeof(Color),
-                typeof(FancyFrame),
-                Color.Transparent,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.OuterBackgroundColor = (Color)newValue;
-                });
+        public static readonly BindableProperty BGColorProperty =
+           BindableProperty.Create(nameof(BGColor), typeof(Color), typeof(FancyFrame), Color.Transparent);
 
-        public Color OuterBackgroundColor
+        public Color BGColor
         {
-            get { return (Color)GetValue(OuterBackgroundColorProperty); }
-            set
-            {
-                SetValue(OuterBackgroundColorProperty, value);
-                thisOuterBackgroundColor = value.ToSKColor();
-                InvalidateSurface();
-            }
+            get { return (Color)GetValue(BGColorProperty); }
+            set { SetValue(BGColorProperty, value); }
         }
 
-        private SKColor thisOuterBackgroundColor;
-        #endregion
-
-        #region Inner Start
-        public static readonly BindableProperty InnerBackgroundColorStartProperty =
-            BindableProperty.Create("InnerBackgroundColorStart",
-                typeof(Color),
-                typeof(FancyFrame),
-                Color.Red,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.InnerBackgroundColorStart = (Color)newValue;
-                });
-
-        public Color InnerBackgroundColorStart
-        {
-            get { return (Color)GetValue(InnerBackgroundColorStartProperty); }
-            set
-            {
-                SetValue(InnerBackgroundColorStartProperty, value);
-                thisInnerBackgroundColorStart = value.ToSKColor();
-                InvalidateSurface();
-            }
-        }
-
-        private SKColor thisInnerBackgroundColorStart;
-        #endregion
-
-        #region Inner End
-        public static readonly BindableProperty InnerBackgroundColorEndProperty =
-            BindableProperty.Create(nameof(InnerBackgroundColorEnd),
-                typeof(Color),
-                typeof(FancyFrame),
-                Color.Red,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.InnerBackgroundColorEnd = (Color)newValue;
-                });
-
-        public Color InnerBackgroundColorEnd
-        {
-            get { return (Color)GetValue(InnerBackgroundColorEndProperty); }
-            set
-            {
-                SetValue(InnerBackgroundColorEndProperty, value);
-                thisInnerBackgroundColorEnd = value.ToSKColor();
-                InvalidateSurface();
-            }
-        }
-
-        private SKColor thisInnerBackgroundColorEnd;
-        #endregion
-
-        #region Border Color
+        
         public static readonly BindableProperty BorderColorProperty =
-            BindableProperty.Create(nameof(BorderColor),
-                typeof(Color),
-                typeof(FancyFrame),
-                Color.Blue,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.BorderColor = (Color)newValue;
-                });
+            BindableProperty.Create(nameof(BorderColor), typeof(Color),typeof(FancyFrame), Color.Black);
 
         public Color BorderColor
         {
             get { return (Color)GetValue(BorderColorProperty); }
-            set
-            {
-                SetValue(BorderColorProperty, value);
-                thisBorderColor = value.ToSKColor();
-                InvalidateSurface();
-            }
+            set { SetValue(BorderColorProperty, value); }
         }
-
-        private SKColor thisBorderColor;
-        #endregion
-
-        #region Border Width
-        public static readonly BindableProperty BorderWidthProperty =
-            BindableProperty.Create(nameof(BorderWidth),
-                typeof(int),
-                typeof(FancyFrame),
-                1,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.BorderWidth = (int)newValue;
-                });
+        
+        public static readonly BindableProperty BorderWidthProperty = BindableProperty.Create(nameof(BorderWidth),typeof(int), typeof(FancyFrame), 1);
 
         public int BorderWidth
         {
             get { return (int)GetValue(BorderWidthProperty); }
-            set
-            {
-                SetValue(BorderWidthProperty, value);
-                InvalidateSurface();
-            }
+            set { SetValue(BorderWidthProperty, value); }
         }
-        #endregion
-
-        #region Border Radius
-        public static readonly BindableProperty BorderRadiusProperty =
-            BindableProperty.Create(nameof(BorderRadius),
-                typeof(float),
-                typeof(FancyFrame),
-                25f,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.BorderRadius = (float)newValue;
-                });
+        
+        public static readonly BindableProperty BorderRadiusProperty = BindableProperty.Create(nameof(BorderRadius),typeof(float), typeof(FancyFrame),25f );
 
         public float BorderRadius
         {
             get { return (float)GetValue(BorderRadiusProperty); }
-            set
-            {
-                SetValue(BorderRadiusProperty, value);
-                InvalidateSurface();
-            }
+            set { SetValue(BorderRadiusProperty, value); }
         }
-        #endregion
-
-        #region Border Margin
-        public static readonly BindableProperty BorderMarginProperty =
-            BindableProperty.Create(nameof(BorderMargin),
-                typeof(float),
-                typeof(FancyFrame),
-                25f,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.BorderMargin = (float)newValue;
-                });
+        
+        public static readonly BindableProperty BorderMarginProperty = BindableProperty.Create(nameof(BorderMargin), typeof(float), typeof(FancyFrame),25f );
 
         public float BorderMargin
         {
             get { return (float)GetValue(BorderMarginProperty); }
-            set
-            {
-                SetValue(BorderMarginProperty, value);
-                InvalidateSurface();
-            }
+            set { SetValue(BorderMarginProperty, value); }
         }
-        #endregion
-
-        #region Border Padding
-        public static readonly BindableProperty BorderPaddingProperty =
-            BindableProperty.Create(nameof(BorderPadding),
-                typeof(float),
-                typeof(FancyFrame),
-                25f,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.BorderPadding = (float)newValue;
-                });
+        
+        public static readonly BindableProperty BorderPaddingProperty = BindableProperty.Create(nameof(BorderPadding), typeof(float), typeof(FancyFrame), 25f );
 
         public float BorderPadding
         {
             get { return (float)GetValue(BorderPaddingProperty); }
-            set
-            {
-                SetValue(BorderPaddingProperty, value);
-                InvalidateSurface();
-            }
-        }
-        #endregion
+            set { SetValue(BorderPaddingProperty, value); }
+        }       
 
-        #region Fill Orientation
-        public enum FillOrientations
+        new public static readonly BindableProperty BackgroundColorProperty =
+                    BindableProperty.Create(nameof(BackgroundColor), typeof(Color),
+                        typeof(FancyFrame), Color.Transparent);
+
+        new public Color BackgroundColor
         {
-            Horizontal,
-            Vertical
+            get => (Color)GetValue(BackgroundColorProperty);
+            set => SetValue(BackgroundColorProperty, value);
         }
-
-        public static readonly BindableProperty FillOrientationProperty =
-            BindableProperty.Create(nameof(FillOrientation),
-                typeof(FillOrientations),
-                typeof(FancyFrame),
-                FillOrientations.Horizontal,
-                propertyChanged: (currentControl, oldValue, newValue) =>
-                {
-                    var thisControl = currentControl as FancyFrame;
-                    thisControl.FillOrientation = (FillOrientations)newValue;
-                });
-
-        public FillOrientations FillOrientation
-        {
-            get { return (FillOrientations)GetValue(FillOrientationProperty); }
-            set
-            {
-                SetValue(FillOrientationProperty, value);
-                InvalidateSurface();
-            }
-        }
-        #endregion
 
         protected override void OnChildAdded(Element child)
         {
-            if (this.Children.Count > 1)
+            if (Children.Count > 1)
             {
                 SetRow(child, 0);
                 SetColumn(child, 0);
                 SetRowSpan(child, 1);
                 SetColumnSpan(child, 1);
-                ((View)child).Margin = BorderMargin + BorderWidth + BorderPadding;
+                //((View)child).Margin = BorderMargin + BorderWidth + BorderPadding;
+                ((View)child).Margin = BorderMargin + BorderPadding;
             }
             base.OnChildAdded(child);
         }
-
-
-        private void InvalidateSurface()
-        {
-            canvas?.InvalidateSurface();
-        }
-
+       
         protected void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
             SKImageInfo info = e.Info;
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
 
-            canvas.Clear(thisOuterBackgroundColor);
+            canvas.Clear();
 
             int width = info.Width;
             int height = info.Height;
@@ -295,40 +120,38 @@ namespace FancyFrameApp.Control
             {
                 Style = SKPaintStyle.Stroke,
                 StrokeWidth = BorderWidth,
-                Color = thisBorderColor,
+                Color = BorderColor.ToSKColor(),
                 IsAntialias = true
             };
             canvas.DrawRoundRect(rect, BorderRadius, BorderRadius, paintBorder);
-
-            var colors = new SKColor[] { thisInnerBackgroundColorStart, thisInnerBackgroundColorEnd };
-            SKShader shader = null;
-            if (FillOrientation.Equals(FillOrientations.Vertical))
-            {
-                shader = SKShader.CreateLinearGradient(
-                    new SKPoint(0, 0),
-                    new SKPoint(0, 100),
-                    colors,
-                    null,
-                    SKShaderTileMode.Clamp);
-            }
-            else
-            {
-                shader = SKShader.CreateLinearGradient(
-                    new SKPoint(0, 0),
-                    new SKPoint(100, 0),
-                    colors,
-                    null,
-                    SKShaderTileMode.Clamp);
-            }
+            
 
             var paintFill = new SKPaint()
-            {
-                Shader = shader,
+            {      
+                Color=BackgroundColor.ToSKColor(),
                 Style = SKPaintStyle.Fill,
                 IsAntialias = true
             };
 
             canvas.DrawRoundRect(rect, BorderRadius, BorderRadius, paintFill);
+            canvas.ClipRect(rect, SKClipOperation.Intersect);
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            // Determine when to change. Basically on any of the properties that we've added that affect
+            // the visualization, including the size of the control, we'll repaint
+            if (propertyName == BGColorProperty.PropertyName ||
+                propertyName == BorderColorProperty.PropertyName ||
+                propertyName == BorderWidthProperty.PropertyName ||
+                propertyName == BorderRadiusProperty.PropertyName ||
+                propertyName == BorderPaddingProperty.PropertyName ||
+                propertyName == BackgroundColorProperty.PropertyName )
+            {
+                canvas.InvalidateSurface();
+            }
         }
     }
 }
