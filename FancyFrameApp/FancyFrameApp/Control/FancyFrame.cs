76f2b8238c8af33e58551d080cb63623e4709b2e
Yuriy Holembyovskyy <yuriy.holembyovskyy@gmail.com>
2020-04-17 18:36:54 +0300

    Added BackgroundGradientAngle Added BackgroundGradientEndColor Added BackgroundGradientStartColor

--- FancyFrameApp/FancyFrameApp/Control/FancyFrame.cs
+++ FancyFrameApp/FancyFrameApp/Control/FancyFrame.cs
@@ -35,14 +35,19 @@ namespace FancyFrameApp.Control
                                                                                                         FancyFrame shadowView = bindableObject as FancyFrame;
                                                                                                         shadowView.contentGrid.Children.Add((View)newValue);
                                                                                                     });
-        new public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(FancyFrame), Color.Transparent);
+        new public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(FancyFrame), Color.White);
         public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(FancyFrame), Color.Black);
-        public static readonly BindableProperty BorderWidthProperty = BindableProperty.Create(nameof(BorderWidth), typeof(int), typeof(FancyFrame), 1);
-        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(FancyFrame), 25f);
+        public static readonly BindableProperty BorderWidthProperty = BindableProperty.Create(nameof(BorderWidth), typeof(int), typeof(FancyFrame), 0);
         public static readonly BindableProperty BorderMarginProperty = BindableProperty.Create(nameof(BorderMargin), typeof(float), typeof(FancyFrame), 25f);
         public static readonly BindableProperty BorderPaddingProperty = BindableProperty.Create(nameof(BorderPadding), typeof(float), typeof(FancyFrame), 25f);
+
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(FancyFrame), 25f);
         public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(FancyFrame), default(bool));
         public static readonly BindableProperty ShadowColorProperty = BindableProperty.Create(nameof(ShadowColor), typeof(Color), typeof(FancyFrame), Color.Black);
+
+        public static readonly BindableProperty BackgroundGradientStartColorProperty = BindableProperty.Create(nameof(BackgroundGradientStartColor), typeof(Color), typeof(FancyFrame), defaultValue: default(Color));
+        public static readonly BindableProperty BackgroundGradientEndColorProperty = BindableProperty.Create(nameof(BackgroundGradientEndColor), typeof(Color), typeof(FancyFrame), defaultValue: default(Color));
+        public static readonly BindableProperty BackgroundGradientAngleProperty = BindableProperty.Create(nameof(BackgroundGradientAngle), typeof(int), typeof(FancyFrame), defaultValue: default(int));
 
 
 
@@ -108,6 +113,24 @@ namespace FancyFrameApp.Control
             set { SetValue(ShadowColorProperty, value); }
         }
 
+        public Color BackgroundGradientStartColor
+        {
+            get { return (Color)GetValue(BackgroundGradientStartColorProperty); }
+            set { SetValue(BackgroundGradientStartColorProperty, value); }
+        }
+
+        public Color BackgroundGradientEndColor
+        {
+            get { return (Color)GetValue(BackgroundGradientEndColorProperty); }
+            set { SetValue(BackgroundGradientEndColorProperty, value); }
+        }
+
+        public int BackgroundGradientAngle
+        {
+            get { return (int)GetValue(BackgroundGradientAngleProperty); }
+            set { SetValue(BackgroundGradientAngleProperty, value); }
+        }
+
         protected void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
             SKImageInfo info = e.Info;
@@ -119,34 +142,55 @@ namespace FancyFrameApp.Control
             int width = info.Width;
             int height = info.Height;
 
+            //General Frame rect
             SKRect rect = new SKRect
             {
                 Left = BorderMargin,
                 Top = BorderMargin,
                 Right = width - BorderMargin,
                 Bottom = height - BorderMargin
-            };            
-            
-
-            SKPaint paintBorder = new SKPaint
-            {
-                Style = SKPaintStyle.Stroke,
-                StrokeWidth = BorderWidth,
-                Color = BorderColor.ToSKColor(),
-                IsAntialias = true
             };
-            canvas.DrawRoundRect(rect, CornerRadius, CornerRadius, paintBorder);
 
+            #region Border
+            if (BorderWidth > 0)
+            {
+                SKPaint paintBorder = new SKPaint
+                {
+                    Style = SKPaintStyle.Stroke,
+                    StrokeWidth = BorderWidth,
+                    Color = BorderColor.ToSKColor(),
+                    IsAntialias = true
+                };
+                canvas.DrawRoundRect(rect, CornerRadius, CornerRadius, paintBorder);
+            }
+            #endregion
 
-            var paintFill = new SKPaint()
+            var backgroundPaint = new SKPaint()
             {
                 Color = BackgroundColor.ToSKColor(),
                 Style = SKPaintStyle.Fill,
                 IsAntialias = true,
                 ImageFilter = HasShadow ? SKImageFilter.CreateDropShadow(0f, 0f, 20f, 20f, ShadowColor.ToSKColor(), SKDropShadowImageFilterShadowMode.DrawShadowAndForeground, null, null) : null
             };
+            
+            if (BackgroundGradientStartColor != Color.Default && BackgroundGradientEndColor != Color.Default)
+            {                
+                var angle = BackgroundGradientAngle / 360.0;
+                // Calculate the new positions based on angle between 0-360.
+                var a = width * Math.Pow(Math.Sin(2 * Math.PI * ((angle + 0.75) / 2)), 2);
+                var b = height * Math.Pow(Math.Sin(2 * Math.PI * ((angle + 0.0) / 2)), 2);
+                var c = width * Math.Pow(Math.Sin(2 * Math.PI * ((angle + 0.25) / 2)), 2);
+                var d = height * Math.Pow(Math.Sin(2 * Math.PI * ((angle + 0.5) / 2)), 2);
+                
+                backgroundPaint.Shader = SKShader.CreateLinearGradient(
+                               new SKPoint(width - (float)a, (float)b),
+                               new SKPoint(width - (float)c, (float)d),                                
+                               new SKColor[] {BackgroundGradientStartColor.ToSKColor(), BackgroundGradientEndColor.ToSKColor() },
+                               null,
+                               SKShaderTileMode.Clamp);
+            }
 
-            canvas.DrawRoundRect(rect, CornerRadius, CornerRadius, paintFill);
+            canvas.DrawRoundRect(rect, CornerRadius, CornerRadius, backgroundPaint);
             canvas.ClipRect(rect, SKClipOperation.Intersect);
         }
 
@@ -162,7 +206,10 @@ namespace FancyFrameApp.Control
                 propertyName == BorderPaddingProperty.PropertyName ||
                 propertyName == BackgroundColorProperty.PropertyName ||
                 propertyName == HasShadowProperty.PropertyName ||
-                propertyName == ShadowColorProperty.PropertyName)
+                propertyName == ShadowColorProperty.PropertyName ||
+                propertyName == BackgroundGradientStartColorProperty.PropertyName ||
+                propertyName == BackgroundGradientEndColorProperty.PropertyName ||
+                propertyName == BackgroundGradientAngleProperty.PropertyName)
             {
                 canvas.InvalidateSurface();
             }
