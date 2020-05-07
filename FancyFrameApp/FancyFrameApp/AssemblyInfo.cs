e1f8c5a443fd1e2942a18ef5b8a9a5ac8fc0cef4
Yuriy Holembyovskyy <yuriy.holembyovskyy@gmail.com>
2020-05-07 14:12:42 +0300

    Added Embedded Fonts Added Embedded images for common usage

--- FancyFrameApp/FancyFrameApp/AssemblyInfo.cs
+++ FancyFrameApp/FancyFrameApp/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Lato-Bold.ttf", Alias = "LatoBold")]
+[assembly: ExportFont("Lato-Regular.ttf", Alias = "LatoRegular")]
